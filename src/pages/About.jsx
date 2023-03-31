b787595c377a4d84ab333a242276c9511194fa7a
Vaibhao-H <vaibhaohatwar.works@gmail.com>
2023-03-31 18:19:32 +0530

    Navigate component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,13 @@
+import { useState } from "react"
+import { Navigate } from "react-router-dom"
+
 export default function About() {
+  const [user, setUser] = useState("mario")
+
+  if (!user) {
+    return <Navigate to="/" replace={true} />
+  }
+
   return (
     <div className="about">
       <h2>About Us</h2>
@@ -17,6 +26,7 @@ export default function About() {
         eligendi ipsa voluptatem voluptas aliquid fuga, molestiae ex natus
         necessitatibus sapiente amet ducimus vel nam reprehenderit molestias!
       </p>
+      <button onClick={() => setUser(null)}>Logout</button>
     </div>
   )
 }
