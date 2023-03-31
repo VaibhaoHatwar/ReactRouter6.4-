7de2dc3ba58865eab399eb9dbf1f54e4da04935c
Vaibhao-H <vaibhaohatwar.works@gmail.com>
2023-03-31 11:33:58 +0530

    Added Root Layout and get started with react router 6.4+

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,28 @@
-import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom"
+import {
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+  createRoutesFromElements,
+} from "react-router-dom"
 
 // pages
 import AboutPage from "./pages/About"
 import HomePage from "./pages/Home"
 
-function App() {
-  return (
-    <BrowserRouter>
-      <header>
-        <nav>
-          <h1>ReactRouter6.4+</h1>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/about">About</NavLink>
-        </nav>
-      </header>
-      <main>
-        <Routes>
-          <Route index element={<HomePage />} />
-          <Route path="about" element={<AboutPage />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
+// layouts
+import RootLayout from "./layout/RootLayout"
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path="about" element={<AboutPage />} />
+    </Route>
   )
+)
+
+function App() {
+  return <RouterProvider router={router} />
 }
 
 export default App
