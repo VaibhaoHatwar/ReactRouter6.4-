f8527d2dc24ad4146b6a8cffad2ea9b86d6e9230
Vaibhao-H <vaibhaohatwar.works@gmail.com>
2023-03-31 13:58:46 +0530

    Added loader function inside routes to frtch data from API's

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import {
   createBrowserRouter,
+  createRoutesFromElements,
   Route,
   RouterProvider,
-  createRoutesFromElements,
 } from "react-router-dom"
 
 // pages
-import About from "./pages/About"
 import Home from "./pages/Home"
+import About from "./pages/About"
 import Faq from "./pages/help/Faq"
 import Contact from "./pages/help/Contact"
+import NotFound from "./pages/NotFound"
+import Careers, { careersLoader } from "./pages/careers/Careers"
 
 // layouts
-import RootLayout from "./layout/RootLayout"
-import HelpLayout from "./layout/HelpLayout"
-import NotFound from "./pages/NotFound"
+import RootLayout from "./layouts/RootLayout"
+import CareersLayout from "./layouts/CareersLayout"
+import HelpLayout from "./layouts/HelpLayout"
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -24,6 +26,9 @@ const router = createBrowserRouter(
       <Route path="help" element={<HelpLayout />}>
         <Route path="faq" element={<Faq />} />
         <Route path="contact" element={<Contact />} />
+      </Route>
+      <Route path="careers" element={<CareersLayout />}>
+        <Route index element={<Careers />} loader={careersLoader} />
       </Route>
 
       <Route path="*" element={<NotFound />} />
